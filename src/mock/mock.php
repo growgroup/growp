7b365f0652e5398257b018c8c29e197895def639
1shiharat <akeome1369@gmail.com>
2017-04-04 10:24:54 +0900

    tinymce advanced と wp admin ui customize のインストール時の設定を自動化

--- src/mock/mock.php
+++ src/mock/mock.php
@@ -7,6 +7,8 @@ require_once dirname(__FILE__) . "/frontandhome-mock.php";
 require_once dirname(__FILE__) . "/mw-wp-form-mock.php";
 require_once dirname(__FILE__) . "/privacy-policy-mock.php";
 require_once dirname(__FILE__) . "/sitemap-mock.php";
+require_once dirname(__FILE__) . "/wp-admin-ui-customize-mock.php";
+require_once dirname(__FILE__) . "/tinymce-advanced-mock.php";
 
 add_action("after_switch_theme", "growp_mock_init");
 function growp_mock_init()
@@ -15,4 +17,6 @@ function growp_mock_init()
     new GROWP_Mw_Wp_Form_Mock();
     new GROWP_FrontAndHome_Mock();
     new GROWP_Privacy_Policy_Mock();
+    new GROWP_WP_Admin_Ui_Customize_Mock();
+    new GROWP_Tinymce_Advanced_Mock();
 }
