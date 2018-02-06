4692cf417e6c0a1b221511d6b1adc4b816e7fac7
ishihara takashi <akeome1369@gmail.com>
2018-02-06 14:10:52 +0900

    Update setup.php

--- src/hooks/setup.php
+++ src/hooks/setup.php
@@ -137,3 +137,21 @@ function growp_filter_editable_roles( $all_roles ) {
 }
 
 add_filter( "editable_roles", "growp_filter_editable_roles" );
+
+/**
+ * TinyMCEのspanタグ等の自動削除を停止
+ */
+function growp_override_mce_options( $init_array ) {
+	global $allowedposttags;
+
+	$init_array['valid_elements']          = '*[*]';
+	$init_array['extended_valid_elements'] = '*[*]';
+	$init_array['valid_children']          = '+a[' . implode( '|', array_keys( $allowedposttags ) ) . ']';
+	$init_array['indent']                  = true;
+	$init_array['wpautop']                 = false;
+	$init_array['force_p_newlines']        = false;
+
+	return $init_array;
+}
+
+add_filter( 'tiny_mce_before_init', 'growp_override_mce_options' );
