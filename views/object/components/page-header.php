a2bc7b473617b8fa8b29718066c7dd063c71523a
ishihara takashi <akeome1369@gmail.com>
2018-02-16 20:07:54 +0900

    Update page-header.php

--- views/object/components/page-header.php
+++ views/object/components/page-header.php
@@ -14,13 +14,20 @@ if ( is_front_page() ) {
 }
 
 global $post;
-$title    = apply_filters( 'growp/page_header/title', get_the_title() );
-$subtitle = apply_filters( 'growp/page_header/subtitle', mb_strtoupper( $post->post_name ) );
-$image    = apply_filters( 'growp/page_header/image', GUrl::asset( '/assets/images/bg-page-header' . $post->post_name . '.jpg' ) );
+$title       = apply_filters( 'growp/page_header/title', get_the_title() );
+$subtitle    = apply_filters( 'growp/page_header/subtitle', mb_strtoupper( $post->post_name ) );
+$image       = apply_filters( 'growp/page_header/image', GUrl::asset( '/assets/images/bg-page-header-' . $post->post_name . '.jpg' ) );
+$pageheaders = apply_filters( 'growp/page_header', array(
+	'title'    => $title,
+	'subtitle' => $subtitle,
+	'image'    => $image,
+) );
 ?>
-<header class="c-page-header" style="background-image: url(<?php $image ?>)">
-	<h1 class="heading is-xlg c-page-header__text">
-		<?php echo $title ?>
-	</h1>
-	<p><?php echo $subtitle ?></p>
-</header>
+<!-- ページヘッダー-->
+<div class="c-page-header" style="background-image: url(<?php echo $pageheaders["image"] ?>)">
+	<div class="c-page-header__inner">
+		<h1><?php echo $pageheaders["title"] ?>
+			<small><?php echo $pageheaders["subtitle"] ?></small>
+		</h1>
+	</div>
+</div>
