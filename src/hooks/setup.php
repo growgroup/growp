96f01c0e48c86c6e02669315ba94263306428374
ishihara takashi <akeome1369@gmail.com>
2018-02-23 20:45:38 +0900

    Update setup.php

--- src/hooks/setup.php
+++ src/hooks/setup.php
@@ -156,27 +156,6 @@ function growp_override_mce_options( $init_array ) {
 
 add_filter( 'tiny_mce_before_init', 'growp_override_mce_options' );
 
-
-/**
- * TinyMCEのspanタグ等の自動削除を停止
- *
- * @param $init_array
- */
-function growp_override_mce_options( $init_array ) {
-	global $allowedposttags;
-
-	$init_array['valid_elements']          = '*[*]';
-	$init_array['extended_valid_elements'] = '*[*]';
-	$init_array['valid_children']          = '+a[' . implode( '|', array_keys( $allowedposttags ) ) . ']';
-	$init_array['indent']                  = true;
-	$init_array['wpautop']                 = false;
-	$init_array['force_p_newlines']        = false;
-
-	return $init_array;
-}
-
-add_filter( 'tiny_mce_before_init', 'growp_override_mce_options' );
-
 /**
  * ページヘッダーのフィルター
  *
