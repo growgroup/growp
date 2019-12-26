c678e7b7ba6afe63f04fa42dc66e5cd8dff0d144
takashiono-gg <44341515+takashiono-gg@users.noreply.github.com>
2019-12-26 16:45:23 +0900

    デザインシステムのデフォルトコーディングに合わせて修正

--- views/object/components/breadcrumb.php
+++ views/object/components/breadcrumb.php
@@ -9,5 +9,5 @@
 
 // WordPress SEO by Yoast のパンくずを利用する
 if ( function_exists( 'yoast_breadcrumb' ) ) {
-	yoast_breadcrumb( '<div class="c-breadcrumb">', '</div>' );
+	yoast_breadcrumb( '<div class="c-breadcrumb"><div class="l-container"><div class="c-breadcrumb__inner">', '</div></div></div>' );
 }
