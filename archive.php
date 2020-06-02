3729e9955c154eeab41477939a60c31499f96bf8
takashiono-gg <44341515+takashiono-gg@users.noreply.github.com>
2020-06-02 18:16:58 +0900

    最近のHTMLに合わせて調整

--- archive.php
+++ archive.php
@@ -8,65 +8,81 @@
  * @see http://codex.wordpress.org/Template_Hierarchy
  * =====================================================
  */
+global $post;
+$cat_slug    = "";
+$current_cat = get_queried_object();
+if ( $current_cat ) {
+	$cat_slug = $current_cat->slug;
+}
+$terms     = GTag::get_all_term( "category", "", true );
+$all_class = $cat_slug == "" ? "is-active" : "";
 ?>
-<section class="l-section is-lg">
+<section class="l-section is-lower-case">
 	<div class="l-container">
-		<div class="c-tabs  is-index">
-			<ul class="c-tabs__navs">
-				<li>
-					<a <?php echo ( is_home() ) ? 'class="is-active"' : "" ?> href="<?php echo home_url( "/column/" ) ?>">すべて</a>
-				</li>
-				<?php
-				get_terms([
-					'taxonomy' => 'category'
-				]);
-				$current_category = get_queried_object();
-				foreach ( $items as $item ) {
-					$active_class = "";
-					if ( $item->term_id === $current_category->term_id ) {
-						$active_class = "is-active";
-					}
-					?>
-					<li>
-						<a class="<?php echo $active_class ?>" href="<?php echo $item->get_archive_link(); ?>"><?php echo $item->get_name(); ?></a>
-					</li>
-					<?php
-				}
-				?>
-			</ul>
-		</div>
-		<div class="u-mbs is-top is-lg">
-			<div class="c-card-post  is-tag-hidden">
-				<?php
-				if ( have_posts() ) {
-					?>
-					<div class="row">
-						<?php
-						while ( have_posts() ) {
-							?>
-							<div class="large-4 small-6">
-								<?php
-								the_post();
-								GTemplate::get_component( "card-post-block" );
-								?>
+		<div class="row">
+			<div class="large-10 is-push-lg-1 small-12">
+				<div class="u-mbs is-bottom is-lg">
+					<div class="c-box-archive">
+						<div class="c-box-archive__block">
+							<div class="c-box-archive__title">カテゴリ
 							</div>
-							<?php
-						}
+							<ul>
+								<li>
+									<a class=" <?php echo $all_class; ?>" href="<?php echo get_post_type_archive_link( 'post' ); ?>"><span>すべて</span></a>
+								</li>
+								<?php
+								$count = 1;
+								foreach ( $terms as $term ) {
+									$class = "";
+									$title = esc_html( $term->name );
+									$link  = get_term_link( $term );
+									$class = $cat_slug == $term->slug ? "is-active" : "";
+									echo "<li><a class=\" {$class}\" href=\"{$link}\" ><span>{$title}</span></a></li>";
+									$count ++;
+								}
+								?>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="c-news  is-onecolumn">
+			<div class="row">
+				<div class="large-10 is-push-lg-1 small-12">
+					<div class="c-news__content">
+						<?php
+						if ( have_posts() ) :
+							/* ループをスタート */
+							while ( have_posts() ) : the_post();
+								GTemplate::get_project( "post-item" );
+							endwhile;
+						else :
+							get_template_part( 'content', 'none' );
+						endif;
 						?>
 					</div>
 					<?php
-				} ?>
-			</div>
-			<?php echo GNav::get_paging_nav(); ?>
-			<div class="c-box-archive">
-				<div class="c-box-archive__block">
-					<div class="c-box-archive__title">年月アーカイブ
+					echo GNav::get_paging_nav();
+					?>
+					<div class="u-mbs is-top is-lg">
+						<div class="c-box-archive">
+							<div class="c-box-archive__block">
+								<div class="c-box-archive__title">年月アーカイブ
+								</div>
+								<ul>
+									<?php
+									$archive_str = wp_get_archives( [
+										'type' => "monthly",
+										'echo' => false
+									] );
+
+									echo $archive_str;
+									?>
+								</ul>
+							</div>
+						</div>
 					</div>
-					<ul>
-						<?php
-						wp_get_archives();
-						?>
-					</ul>
 				</div>
 			</div>
 		</div>
