7f8930719d76428716349f76a0187dd256ac2cb3
takashiono-gg <44341515+takashiono-gg@users.noreply.github.com>
2020-06-02 18:14:29 +0900

    Update single.php

--- single.php
+++ single.php
@@ -9,36 +9,56 @@
  * =====================================================
  */
 
-while ( have_posts() ) :
-	the_post();
-	?>
-	<div class="l-container">
-		<article id="post-<?php the_ID(); ?>" <?php post_class( 'entry' ); ?>>
+the_post();
+?>
 
-			<h1 class="heading is-xlg"><?php the_title(); ?></h1>
-			<div class="p-post__meta">
-				<?php echo GTag::get_the_terms_label_list() ?>
-			</div>
-			<div class="l-post-content">
+<section class="l-section is-lg">
+	<div class="l-container">
+		<div class="row">
+			<div class="large-8 is-push-lg-2 small-12">
+				<div class="c-news-header">
+					<h1 class="c-news-header__title">
+						<?php the_title(); ?>
+					</h1>
+					<div class="c-news-header__sup">
+						<div class="c-news-header__label">
+							<?php echo GTag::get_first_term( get_the_ID(), "category", "name" ) ?>
+						</div>
+						<div class="c-news-header__date">
+							<?php the_time( "Y.m.d" ); ?>
+						</div>
+					</div>
+				</div>
 				<?php
-				the_content();
-				wp_link_pages( array(
-					'before' => '<div class="page-links">' . __( 'Pages:', 'growp' ),
-					'after'  => '</div>',
-				) );
+				if ( has_post_thumbnail() ) {
+					?>
+					<div class="u-text-center u-mbs is-sm">
+						<?php the_post_thumbnail( 'large' ); ?>
+					</div>
+					<?php
+				}
+				?>
+				<div class="l-post-content u-mbs is-sm">
+					<?php
+					the_content();
+					wp_link_pages( array(
+						'before' => '<div class="page-links">' . __( 'Pages:', 'growp' ),
+						'after'  => '</div>',
+					) );
+					?>
+				</div>
+				<div class="u-mbs is-bottom">
+					<div class="c-button-social">
+						<?php if ( function_exists( 'ADDTOANY_SHARE_SAVE_KIT' ) ) {
+							ADDTOANY_SHARE_SAVE_KIT();
+						} ?>
+					</div>
+				</div>
+				<hr class="c-hr">
+				<?php
+				GNav::the_post_nav();
 				?>
 			</div>
-		</article><!-- #post-## -->
-
-		<?php
-		GNav::the_post_nav();
-		?>
+		</div>
 	</div>
-	<?php
-	if ( ( comments_open() || '0' != get_comments_number() ) ) {
-		comments_template();
-	}
-	?>
-<?php
-
-endwhile; // end of the loop.
+</section>
