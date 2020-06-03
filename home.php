b00713519900466f4f4ff45c40dbb1bec98b9625
takashiono-gg <44341515+takashiono-gg@users.noreply.github.com>
2020-06-03 10:59:56 +0900

    HTMLの実装に合わせて調整しました

--- home.php
+++ home.php
@@ -8,22 +8,4 @@
  * @see http://codex.wordpress.org/Template_Hierarchy
  * =====================================================
  */
-
-if ( have_posts() ) :
-	?>
-	<div class="l-container">
-		<?php
-		while ( have_posts() ) :
-			the_post();
-			GTemplate::get_project( "post-item" );
-		endwhile;
-		?>
-	</div>
-
-	<?php
-	// ページネーション
-	echo GNav::get_paging_nav();
-else :
-	get_template_part( 'templates/content', 'none' );
-endif;
-
+get_template_part("archive");
