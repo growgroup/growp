38d7c2cd8c8909968cdacdd4fd79b6648532a7b3
takashiono-gg <44341515+takashiono-gg@users.noreply.github.com>
2020-06-30 13:20:24 +0900

    MW WP FORM に日本語の入力チェックのチェックボックスを追加する

--- src/hooks/extras.php
+++ src/hooks/extras.php
@@ -76,3 +76,70 @@ function growp_shortcode_get_component( $atts ) {
 	return $content;
 }
 add_shortcode( 'growp_component', 'growp_shortcode_get_component' );
+
+
+/**
+ * MW WP FORM に日本語の入力チェックのチェックボックスを追加する
+ */
+if ( class_exists( "MW_WP_Form_Abstract_Validation_Rule" ) ) {
+
+	class JapaneseValidation extends \MW_WP_Form_Abstract_Validation_Rule {
+		/**
+		 * バリデーションルール名を指定
+		 * @var string
+		 */
+		protected $name = 'japanese';
+
+		/**
+		 * バリデーションチェック
+		 *
+		 * @param string $key name属性
+		 * @param array $option
+		 *
+		 * @return string エラーメッセージ
+		 */
+		public function rule( $key, array $options = array() ) {
+			$value = $this->Data->get( $key );
+			if ( ! \MWF_Functions::is_empty( $value ) ) {
+				if ( preg_match( "/(й|ц|у|к|е|н|г|ш|щ|з|х|ъ|ф|ы|в|а|п|р|о|л|д|ж|э|я|ч|с|м|и|т|ь|б|ю|П)/", $value, $matches ) ) {
+					$defaults = array(
+						'message' => "キリル文字は含むことができません。"
+					);
+					$options  = array_merge( $defaults, $options );
+
+					return $options['message'];
+				}
+				// １文字以上日本語が含まれているか？
+				if ( ! preg_match( "/[一-龠]+|[ぁ-ん]+|[ァ-ヴー]+|[一-龠]+|[ａ-ｚＡ-Ｚ０-９]/u", $value ) ) {
+
+					$defaults = array(
+						'message' => "日本語での入力をお願いします"
+					);
+					$options  = array_merge( $defaults, $options );
+
+					return $options['message'];
+				}
+			}
+		}
+
+		/**
+		 * 設定パネルに追加
+		 *
+		 * @param numeric $key バリデーションルールセットの識別番号
+		 * @param array $value バリデーションルールセットの内容
+		 */
+		public function admin( $key, $value ) {
+			?>
+			<label><input type="checkbox" <?php checked( $value[ $this->getName() ],
+					1 ); ?> name="<?php echo MWF_Config::NAME; ?>[validation][<?php echo $key; ?>][<?php echo esc_attr( $this->getName() ); ?>]" value="1" /><?php esc_html_e( '日本語チェック',
+					'mw-wp-form' ); ?></label>
+			<?php
+		}
+	}
+
+	add_filter( "mwform_validation_rules", function ( $validation_rules ) {
+		$validation_rules["japanese"] = new JapaneseValidation();
+
+		return $validation_rules;
+	} );
+}
