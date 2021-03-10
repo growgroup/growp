229a98cdff59139f6e50b63b56c1785b41ac4356
takashiono-gg <t.ono@grow-group.jp>
2021-03-10 16:10:03 +0900

    関連記事表示

--- single.php
+++ single.php
@@ -38,6 +38,21 @@ the_post();
 					<?php
 				}
 				?>
+				<?php
+				if ( GTag::is_external_info() ) {
+					?>
+					<div class="u-text-center u-mbs is-xxlg">
+
+						<a class="c-button is-lg is-file" href="<?php echo GTag::get_link_url(); ?>" target="<?php echo GTag::get_link_target(); ?>">
+							<?php
+							echo GTag::get_link_label();
+							?>
+						</a>
+
+					</div>
+					<?php
+				}
+				?>
 				<div class="l-post-content u-mbs is-sm">
 					<?php
 					the_content();
@@ -58,6 +73,31 @@ the_post();
 				<?php
 				GNav::the_post_nav();
 				?>
+
+				<?php
+				if ( function_exists( "yarpp_get_related" ) ):
+					$related_posts = yarpp_get_related( array( "post_type" => "post" ) );
+					if ( $related_posts ) :
+						?>
+						<div class="u-mbs is-top is-lg">
+							<div class="u-mbs is-bottom is-sm">
+								<h2 class="c-heading is-lg is-border"><span>関連コンテンツ</span></h2>
+							</div>
+							<div class="c-card-post  is-tag-hidden">
+								<div class="row">
+									<?php
+									global $post;
+									foreach ( $related_posts as $post ) :
+										setup_postdata( $post );
+										GTemplate::get_project( "post-item" );
+									endforeach;
+									wp_reset_postdata();
+									?>
+								</div>
+							</div>
+						</div>
+					<?php endif; ?>
+				<?php endif; ?>
 			</div>
 		</div>
 	</div>
