91c8a349cb1a78539f0ba86280d42dac6d7da7fc
takashiono-gg <44341515+takashiono-gg@users.noreply.github.com>
2021-03-11 11:47:48 +0900

    Update footer.php

--- views/layout/footer.php
+++ views/layout/footer.php
@@ -7,12 +7,18 @@
  * @package growp
  * @since 1.0.0
  */
+?>
 
+<?php GTemplate::get_component( "offer"); ?>
+
+<?php
 // 管理画面＞サイトオプションより設定
 // 分岐がある場合は、ショートコード[growp_component name="util/year"]などで記載下さい。
 $footer_html = get_field("o_site_footer","option");
 echo do_shortcode($footer_html);
-wp_footer(); ?>
+?>
+
+<?php wp_footer(); ?>
 
 </body>
 </html>
